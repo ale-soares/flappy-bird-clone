69917fead88ad9d39c07c50e727820f3df30898c
Alexandra Soares <alexandramesquitaa@outlook.com>
2023-04-06 19:08:40 -0300

    feat: build ui

--- Flappy Bird Clone/Assets/Bird.cs
+++ Flappy Bird Clone/Assets/Bird.cs
@@ -6,19 +6,27 @@ public class Bird : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     public float flapStrength;
+    public LogicManager logic;
+    public bool birdIsAlive = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("LogicTag").GetComponent<LogicManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
         {
             myRigidbody.velocity = Vector2.up * flapStrength;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
     }
 }
